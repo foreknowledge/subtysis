a83469cbd6616122614abe98bb92319109148a43
forgreen0624 <forgreen0624@naver.com>
2019-11-12 20:38:52 +0900

    샘플 진입 포인트 셋업

--- app/src/main/java/com/hackday/MainActivity.kt
+++ app/src/main/java/com/hackday/MainActivity.kt
@@ -2,12 +2,44 @@ package com.hackday
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.hackday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        init()
+    }
+
+    private fun init() {
+        binding.startButton.setOnClickListener {
+            showPlayer()
+        }
+    }
+
+    private fun showPlayer() {
+        val playerFragment = PlayerFragment.newInstance()
+        supportFragmentManager
+            .beginTransaction()
+            .add(binding.fragmentContainer.id, playerFragment)
+            .commit()
+    }
+
+    override fun onBackPressed() {
+        supportFragmentManager.findFragmentById(binding.fragmentContainer.id)?.let {
+            supportFragmentManager
+                .beginTransaction()
+                .remove(it)
+                .commit()
+
+            return
+        }
+
+        super.onBackPressed()
     }
 
 }
---
+++ app/src/main/java/com/hackday/PlayerViewModel.kt
@@ -0,0 +1,15 @@
+package com.hackday
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+/**
+ * @author Created by lee.cm on 2019-11-12.
+ */
+class PlayerViewModel : ViewModel() {
+
+    fun test() {
+        Log.d("[Simple-Test]", "PlayerViewModel::test:: ")
+    }
+
+}
