d4958e7cea16b7cdeaa84e88554218580c36ac61
zion830 <zion830@honglab.org>
2019-11-16 18:49:04 +0900

    타입 구분용 enum 클래스 추가 #8

---
+++ subtysis/src/main/java/com/hackday/subtysis/model/SearchType.kt
@@ -0,0 +1,18 @@
+package com.hackday.subtysis.model
+
+enum class SearchType {
+    BLOG,            // 블로그
+    NEWS,            // 뉴스
+    BOOK,            // 책
+    ADULT,           // 성인 검색어 판별
+    ENCYCLOPEDIA,    // 백과사전
+    MOVIE,           // 영화
+    CAFEARTICLE,     // 카페글
+    KIN,             // 지식인
+    LOCAL,           // 지역
+    ERRATA,          // 오타변환
+    WEB,             // 웹문서
+    IMAGE,           // 이미지
+    SHOPPING,        // 쇼핑
+    DOC              // 전문자료
+}
