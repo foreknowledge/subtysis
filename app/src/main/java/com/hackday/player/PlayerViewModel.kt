b3219fbc12e5de04b47fef9ccddc9f5830de0289
zion830 <zion830@honglab.org>
2019-11-21 19:45:17 +0900

    PlayerViewModel에 livedata 추가 #19

--- app/src/main/java/com/hackday/player/PlayerViewModel.kt
+++ app/src/main/java/com/hackday/player/PlayerViewModel.kt
@@ -1,8 +1,10 @@
 package com.hackday.player
 
 import android.net.Uri
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.hackday.subtysis.model.Keyword
 
 /**
  * @author Created by lee.cm on 2019-11-12.
@@ -11,7 +13,13 @@ class PlayerViewModel : ViewModel() {
 
     private val videoSourceUri = MutableLiveData<Uri>()
 
+    private val displayData = MutableLiveData<ArrayList<Keyword>>().apply {
+        this.value = arrayListOf()
+    }
+
     fun getVideoSourceUriLiveData() = videoSourceUri
+
+    fun getDisplayData(): LiveData<ArrayList<Keyword>> = MutableLiveData<ArrayList<Keyword>>()
 
     fun setVideoSourceUri(videoSourceUri: Uri?) {
         this.videoSourceUri.value = videoSourceUri
@@ -21,4 +29,10 @@ class PlayerViewModel : ViewModel() {
         return this.videoSourceUri.value != null
     }
 
+    fun setDisplayData(keywords: ArrayList<Keyword>) {
+        this.displayData.value?.let {
+            it.clear()
+            it.addAll(keywords)
+        }
+    }
 }
