a2639d18ab46d40383b782f50d1418b0b6a44ded
zion830 <zion830@honglab.org>
2019-11-22 04:35:59 +0900

    쇼핑 사진 표시 구현 #19

--- app/src/main/java/com/hackday/player/adapter/PlayerBindingAdapter.kt
+++ app/src/main/java/com/hackday/player/adapter/PlayerBindingAdapter.kt
@@ -2,10 +2,13 @@ package com.hackday.player.adapter
 
 import android.content.Intent
 import android.net.Uri
+import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.hackday.databinding.ItemShoppingBinding
 import com.hackday.subtysis.model.Keyword
@@ -23,7 +26,12 @@ fun bindMetadata(recyclerView: RecyclerView, keywords: ArrayList<Keyword>?) {
     // 임시 데이터
     for (data in 0..10) displayData.add(
         ShoppingItem(
-            "", 1000, 1000, "안녕...", 10000, 10000
+            "https://avatars2.githubusercontent.com/u/54922887?s=200&v=4",
+            1000,
+            1000,
+            "안녕...",
+            10000,
+            10000
         )
     )
 
@@ -54,4 +62,12 @@ fun openUrl(constraintLayout: ConstraintLayout, url: String) {
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
         constraintLayout.context.startActivity(intent)
     }
+}
+
+@BindingAdapter("bind_img")
+fun bindImg(imageView: ImageView, url: String) {
+    Glide.with(imageView.context)
+        .load(url)
+        .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+        .into(imageView)
 }
