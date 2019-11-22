98416add00dc09ab63ed014a246a680fd9cee215
zion830 <zion830@honglab.org>
2019-11-22 14:15:45 +0900

    Merge branch 'develop' of https://github.com/NAVER-CAMPUS-HACKDAY/subtysis into feature/apply_subtysis
    # Conflicts:
    #	app/src/main/java/com/hackday/MainActivity.kt
    #	app/src/main/java/com/hackday/player/PlayerFragment.kt
    #	subtysis/src/main/java/com/hackday/subtysis/MetadataCreatorImpl.java

--- app/src/main/java/com/hackday/player/PlayerFragment.kt
+++ app/src/main/java/com/hackday/player/PlayerFragment.kt
@@ -89,7 +89,6 @@ class PlayerFragment : Fragment() {
                                 break
                             }
                         }
-
                     }
                 }
             }
